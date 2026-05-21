39d3c33ac339f6974849d2220a9fcdc7a2d981a7
quangpham <phamquangdung188@gmail.com>
2026-05-21 07:13:34 +0700

    terve

--- src/main/java/com/example/demo/EcomApplication.java
+++ src/main/java/com/example/demo/EcomApplication.java
@@ -1,6 +1,6 @@
 package com.example.demo;
 
-import jakarta.annotation.PostConstruct;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,12 +17,8 @@ import java.util.TimeZone;
 @EnableScheduling
 public class EcomApplication {
 
-	@PostConstruct
-	public void init() {
-		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
-	}
-
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
 		SpringApplication.run(EcomApplication.class, args);
 	}
 
